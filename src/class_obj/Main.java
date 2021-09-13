fd01b0463ec9c763e258c71deb8087c8cf8e048c
Amitabh <amitabh@Amitabhs-MacBook-Pro.local>
2021-09-13 20:18:27 +0530

    changes

--- src/class_obj/Main.java
+++ src/class_obj/Main.java
@@ -1,5 +1,7 @@
 package class_obj;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,11 +10,11 @@ public class Main {
 //        dog.walk(10);
 //        dog.speak();
 
-        Duck myDuck = new Duck();
+//        Duck myDuck = new Duck();
 //        System.out.println(myDuck.hashCode());
 //        myDuck.fly(5);
 
-        Duck yourDuck = new Duck();
+//        Duck yourDuck = new Duck();
 //        yourDuck.walk(20);
 //        System.out.println(yourDuck.hashCode());
 //        yourDuck.duckAge = 3;
@@ -25,8 +27,35 @@ public class Main {
 //        System.out.println(myDuckAge);
 
 
+//        Duck d1 = new Duck();
+//        Duck d2 = new Duck();
+//        Duck d3 = new Duck();
+//        Duck d4 = new Duck();
 
 
+//        d1.setDuckAge(2, "priya");
+//        d2.setDuckAge(5, "priya");
+//        d3.setDuckAge(7, "Amitabh1");
+//        d4.setDuckAge(30, "priya");
+//
+//        System.out.println(d1.getAge());
+//        System.out.println(d2.getAge());
+//        System.out.println(d3.getAge());
+//        System.out.println(d4.getAge());
+
+        Random dfasdfas = new Random();
+        Duck[] abc = new Duck[5];
+
+        for (int i = 0; i < 5; i++) {
+            Duck temp = new Duck();
+            temp.setDuckAge(dfasdfas.nextInt(150), "Amitabh");
+            abc[i] = temp;
+        }
+
+        for (int i = 0; i < abc.length; i++) {
+            Duck tempDuck = abc[i];
+            System.out.println(tempDuck.getAge());
+        }
 
 
     }
